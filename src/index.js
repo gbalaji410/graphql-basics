3561328b059403f1e318ba4854b626187b7589f4
Balaji Ganesan <balaji2104@gmail.com>
2021-03-01 11:23:49 -0800

    First Commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import myCity,{ message, name } from './myModule'
+
+console.log(message)
+console.log(name)
+console.log(myCity)
---
+++ src/myModule.js
@@ -0,0 +1,6 @@
+//Named Export
+const message = 'Some message from myModule.js file'
+const name = 'Balaji'
+const location = 'San Jose'
+
+export { message, name, location as default }
